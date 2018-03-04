a1c933701651c0122894de7623ab26d5b16903c7
jumtech <despairing.clock+jumGithub@gmail.com>
2018-03-04 13:21:05 +0900

    vueのサンプルアプリ追加

---
+++ public/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{foo}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      foo: 'buzz',
+    }
+  },
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+  color: #2e9afe;
+}
+</style>
---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue/dist/vue.esm.js';
+import App from './components/App.vue';
+const app = new Vue(App);
+app.$mount('#app');
